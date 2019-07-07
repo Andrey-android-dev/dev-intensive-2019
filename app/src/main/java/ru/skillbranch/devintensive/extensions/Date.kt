442232306ba9b711311301419cdae440b867de6c
Andrey <mydev.for.android@gmail.com>
2019-07-07 20:06:04 +0300

    Доделал задания с Date.himanizeDiff() и TimeUnits.plural()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,7 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -25,27 +26,84 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     return this;
 }
 
-fun Date.humanizeDiff(date : Date = Date()):String {
-    //todo home work
-    val curDate = Date()
-    val sec = (curDate.time - date.time) / 1000
-    /*
-    0с - 1с "только что"
-1с - 45с "несколько секунд назад"
-45с - 75с "минуту назад"
-75с - 45мин "N минут назад"
-45мин - 75мин "час назад"
-75мин 22ч "N часов назад"
-22ч - 26ч "день назад"
-26ч - 360д "N дней назад"
->360д "более года назад"
-     */
-    return ""
+fun Date.humanizeDiff(date: Date = Date()): String {
+    val deltaSec: Int = ((this.time - date.time) / 1000).toInt()
+    if (deltaSec < 0) {
+        val deltaAbs = deltaSec.absoluteValue;
+        return when (deltaAbs) {
+            0, 1 -> "только что"
+            in 2..45 -> "несколько секунд назад"
+            in 46..75 -> "минуту назад"
+            in 76..2700 -> "${TimeUnits.MINUTE.plural(deltaAbs / 60)} назад"
+            in 2701..4500 -> "час назад"
+            in 4501..79200 -> "${TimeUnits.HOUR.plural(deltaAbs / 3600)} назад"
+            in 79201..93600 -> "день назад"
+            in 93601..31104000 -> "${TimeUnits.DAY.plural(deltaAbs / 86400)} назад"
+            else -> "более года назад"
+        }
+    } else {
+        return when (deltaSec) {
+            0, 1 -> "только что"
+            in 2..45 -> "через несколько секунд"
+            in 46..75 -> "через минуту"
+            in 76..2700 -> "через ${TimeUnits.MINUTE.plural(deltaSec / 60)}"
+            in 2701..4500 -> "через час"
+            in 4501..79200 -> "через ${TimeUnits.HOUR.plural(deltaSec / 3600)}"
+            in 79201..93600 -> "через день"
+            in 93601..31104000 -> "через ${TimeUnits.DAY.plural(deltaSec / 86400)}"
+            else -> "более чем через год"
+        }
+    }
 }
 
 enum class TimeUnits {
     SECOND,
     MINUTE,
     HOUR,
-    DAY
+    DAY;
+
+    fun plural(value: Int): String {
+        val exc = value % 100;
+        if (exc in 10..20) {
+            return "$value ${getString(Count.MANY, this)}"
+        } else {
+            val lastDigit = value % 10
+            return when (lastDigit) {
+                1 -> "$value ${getString(Count.ONE, this)}"
+                in 2..4 -> "$value ${getString(Count.SOME, this)}"
+                else -> "$value ${getString(Count.MANY, this)}"
+            }
+        }
+    }
+
+    enum class Count {
+        ONE,
+        SOME,
+        MANY
+    }
+
+    private fun getString(count: Count, unit: TimeUnits): String {
+        if (count == Count.ONE) {
+            return when (unit) {
+                SECOND -> "секунду"
+                MINUTE -> "минуту"
+                HOUR -> "час"
+                DAY -> "день"
+            }
+        } else if (count == Count.SOME) {
+            return when (unit) {
+                SECOND -> "секунды"
+                MINUTE -> "минуты"
+                HOUR -> "часа"
+                DAY -> "дня"
+            }
+        } else {
+            return when (unit) {
+                SECOND -> "секунд"
+                MINUTE -> "минут"
+                HOUR -> "часов"
+                DAY -> "дней"
+            }
+        }
+    }
 }
--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -3,14 +3,13 @@ package ru.skillbranch.devintensive
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.core.Is
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThat
 import org.junit.Test
-
-import org.junit.Assert.*
-import ru.skillbranch.devintensive.extensions.TimeUnits
-import ru.skillbranch.devintensive.extensions.add
-import ru.skillbranch.devintensive.extensions.format
-import ru.skillbranch.devintensive.extensions.toUserView
-import ru.skillbranch.devintensive.models.*
+import ru.skillbranch.devintensive.extensions.*
+import ru.skillbranch.devintensive.models.BaseMessage
+import ru.skillbranch.devintensive.models.Chat
+import ru.skillbranch.devintensive.models.User
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
@@ -28,7 +27,7 @@ class ExampleUnitTest {
 
     @Test
     fun test_instance() {
-        val user2 = User("2","John", "Cena")
+        val user2 = User("2", "John", "Cena")
     }
 
     @Test
@@ -47,7 +46,7 @@ class ExampleUnitTest {
 
         fun getUserInfo() = user;
 
-        val(id, firstName, lastName) = getUserInfo()
+        val (id, firstName, lastName) = getUserInfo()
 
         println("$id, $firstName, $lastName")
     }
@@ -56,7 +55,7 @@ class ExampleUnitTest {
     @Test
     fun test_copy() {
         val user = User.makeUser("John Wick")
-        val user2 = user.copy(id="2")
+        val user2 = user.copy(id = "2")
 
         if (user == user2) {
             println("equals ${user.hashCode()} $user \n ${user2.hashCode()} $user2")
@@ -74,14 +73,16 @@ class ExampleUnitTest {
     @Test
     fun test_copy2() {
         val user = User.makeUser("John Wick")
-        val user2 = user.copy(lastVisit = Date().add(-2,TimeUnits.SECOND))
+        val user2 = user.copy(lastVisit = Date().add(-2, TimeUnits.SECOND))
         val user3 = user.copy(lastName = "Cena", lastVisit = Date().add(2, TimeUnits.HOUR))
 
-        println("""
+        println(
+            """
             ${user.lastVisit?.format()}
             ${user2.lastVisit?.format()}
             ${user3.lastVisit?.format()}
-        """.trimIndent())
+        """.trimIndent()
+        )
 
     }
 
@@ -117,27 +118,27 @@ class ExampleUnitTest {
     @Test
     fun test_parseFullName() {
         val fullName = null
-        val result : Pair<String?,String?> = Utils.parseFullName(fullName)
+        val result: Pair<String?, String?> = Utils.parseFullName(fullName)
         assertThat(result.first, Is(nullValue()))
-        assertThat( result.second, Is(nullValue()))
+        assertThat(result.second, Is(nullValue()))
         println(result)
 
         val fullName1 = ""
-        val result1 : Pair<String?,String?> = Utils.parseFullName(fullName1)
+        val result1: Pair<String?, String?> = Utils.parseFullName(fullName1)
         assertThat(result1.first, Is(nullValue()))
-        assertThat( result1.second, Is(nullValue()))
+        assertThat(result1.second, Is(nullValue()))
         println(result1)
 
         val fullName2 = " "
-        val result2 : Pair<String?,String?> = Utils.parseFullName(fullName2)
+        val result2: Pair<String?, String?> = Utils.parseFullName(fullName2)
         assertThat(result2.first, Is(nullValue()))
-        assertThat( result2.second, Is(nullValue()))
+        assertThat(result2.second, Is(nullValue()))
         println(result2)
 
         val fullName3 = "John"
-        val result3 : Pair<String?,String?> = Utils.parseFullName(fullName3)
+        val result3: Pair<String?, String?> = Utils.parseFullName(fullName3)
         assertThat(result3.first, Is(equalTo("John")))
-        assertThat( result3.second, Is(nullValue()))
+        assertThat(result3.second, Is(nullValue()))
         println(result3)
     }
 
@@ -157,18 +158,36 @@ class ExampleUnitTest {
         assertThat(result4, Is(nullValue()))
     }
 
+
+    @Test
+    fun test_humanizeDiff() {
+        assertThat(Date().add(-2, TimeUnits.HOUR).humanizeDiff(), Is(equalTo("2 часа назад")))
+        assertThat(Date().add(-5, TimeUnits.DAY).humanizeDiff(), Is(equalTo("5 дней назад")))
+        assertThat(Date().add(2, TimeUnits.MINUTE).humanizeDiff(), Is(equalTo("через 2 минуты")))
+        assertThat(Date().add(7, TimeUnits.DAY).humanizeDiff(), Is(equalTo("через 7 дней")))
+        assertThat(Date().add(-400, TimeUnits.DAY).humanizeDiff(), Is(equalTo("более года назад")))
+        assertThat(Date().add(400, TimeUnits.DAY).humanizeDiff(), Is(equalTo("более чем через год")))
+    }
+
+    @Test
+    fun test_plural() {
+        assertThat(TimeUnits.SECOND.plural(1), Is(equalTo("1 секунду")))
+        assertThat(TimeUnits.MINUTE.plural(4), Is(equalTo("4 минуты")))
+        assertThat(TimeUnits.HOUR.plural(19), Is(equalTo("19 часов")))
+        assertThat(TimeUnits.DAY.plural(222), Is(equalTo("222 дня")))
+    }
+
     @Test
     fun test_transliteration() {
         val result1 = Utils.transliteration("Женя Стереотипов")
-        assertThat(result1,Is(equalTo("Zhenya Stereotipov")))
+        assertThat(result1, Is(equalTo("Zhenya Stereotipov")))
 
         val result2 = Utils.transliteration("Amazing Петр", "_")
-        assertThat(result2,Is(equalTo("Amazing_Petr")))
+        assertThat(result2, Is(equalTo("Amazing_Petr")))
     }
 
-
     @Test
-    fun test_builder(){
+    fun test_builder() {
         var user = User.Builder()
             .id("1")
             .firstName("Vasya")
@@ -179,18 +198,15 @@ class ExampleUnitTest {
             .lastVisit(Date().add(-2, TimeUnits.DAY))
             .isOnline(true)
             .build()
-
         println(user)
     }
-
 
     @Test
     fun test_time() {
         val date1 = Date()
         val date2 = Date().add(-1, TimeUnits.HOUR)
-        println(date1.time -  date2.time)
+        println(date1.time - date2.time)
     }
-
 
 
 }
